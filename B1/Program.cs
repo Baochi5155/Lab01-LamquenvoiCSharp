b13c0ad84196c30b5140e6ad26a3656610efd73d
Chi Bao <baochi51552305@gmail.com>
2026-09-17 08:05:41 +0700

    Tuan 1/Bai 1/Nhap xuat ho ten

---
+++ B1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace B1
+{
+    public class Program
+    {
+        public void Input()
+        {
+            Console.Write("Input name: ");
+            string? name = Console.ReadLine();
+            Console.WriteLine("Name is: {0}", name);
+        }
+        public static void Main(string[] str)
+        {
+            Program name = new();
+            name.Input();
+        }
+    }
+}
