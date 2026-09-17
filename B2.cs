44d2b323822cd44aa1297f66c4ba82d1df0c2e95
Chi Bao <baochi51552305@gmail.com>
2026-09-17 08:07:13 +0700

    Tuan 1/Bai 2 /Xuat va nhap chuoi

---
+++ B2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace B2
+{
+    public class b2
+    {
+        public void Input()
+        {
+            // Nhắc người dùng nhập họ tên theo định dạng mẫu
+            Console.Write("Nhap ho ten cua ban: ");
+            string? name = Console.ReadLine();
+
+            // In câu chào mừng kèm họ tên 
+            Console.WriteLine("Chao ban {0}!", name);
+        }
+
+        public static void Main(string[] args)
+        {
+            b2 app = new();
+            app.Input();
+        }
+    } 
+}
